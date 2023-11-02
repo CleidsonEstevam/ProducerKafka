14044798a62b8094aa7c290c314c81fdea8b5e77
Cleidson Estevam <cleidsonestevamdasilva@hotmail.com>
2023-11-02 18:20:11 -0300

    V1

---
+++ kafka-producer-simples-csharp/DataPayment.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System;
+
+public class DataPayment
+{
+    public string Name { get; set; }
+    public string CPF { get; set; } 
+    public int Age { get; set; }
+
+    public DataPayment()
+    {
+        Name = "Mayara Cristina Silva Barros";
+        CPF = "10346000000";
+        Age = 28;
+    }
+}
---
+++ kafka-producer-simples-csharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+        using (var p = new ProducerBuilder<Null, string>(config).Build())
+        {
+            try
+            {
+                var count = 0;
+                var dataPayment = new DataPayment();
+
+                var data = JsonSerializer.Serialize(dataPayment);
+                while (true)
+                {
+                    var dr = await p.ProduceAsync("topic-payment-analysis", new Message<Null, string> 
+                    
+                    { Value = $"Mensagem {data} {count++}" });
+                    
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset} | {count}'");
+                    Thread.Sleep(2000);
+                }
+           
+            }
+            catch (ProduceException<Null, string> e)
+            {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+            }
+        }
+    }
+}
